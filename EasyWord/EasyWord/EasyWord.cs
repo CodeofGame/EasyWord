690463fcebe301bdbbbbfb8bb16abbe71dea4a91
CodeOfGame <havefun121456@gmail.com>
2018-01-04 23:52:09 +0800

    生成word文档

---
+++ EasyWord/EasyWord/EasyWord.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MSWord=Microsoft.Office.Interop.Word;
+using System.IO;
+
+namespace EasyWord
+{
+    public class EasyWord:IDisposable
+    {
+        public bool IsOpen { get; set; }
+        public string FileName { get; set; }
+
+        private MSWord._Application wordApp;
+
+        private MSWord._Document wordDoc;
+
+        private static Object missing = System.Reflection.Missing.Value;
+
+        public EasyWord(string filePath)
+        {
+            this.FileName = filePath;
+            //一些初始化的操作
+            wordApp = new MSWord.Application();
+            wordApp.Visible = true;
+            
+        }
+
+        /// <summary>
+        /// 生成word文档
+        /// </summary>
+        /// <param name="directory">目录</param>
+        /// <param name="fileName"></param>
+        /// <returns></returns>
+        public bool CreateWord(string directory)
+        {
+          if(!Directory.Exists(directory))
+                throw new DirectoryNotFoundException();
+
+            wordDoc=wordApp.Documents.Add(ref missing,ref missing,ref missing,ref missing);
+            object fullName=directory+"\\"+this.FileName;
+
+            wordDoc.SaveAs(ref fullName, ref missing, ref missing, ref missing,
+                ref missing, ref missing, ref missing);
+            CloseWord();
+            return true;
+        }
+
+        private void OpenWord()
+        {
+            this.IsOpen = true;
+            if (!File.Exists(this.FileName))
+                throw new FileNotFoundException();
+            object fileName = this.FileName;
+            wordDoc = wordApp.Documents.Open(ref fileName);
+        }
+
+        private void CloseWord()
+        {
+            this.IsOpen = false;
+            wordDoc.Close(MSWord.WdSaveOptions.wdSaveChanges);
+            wordApp.Quit();
+        }
+
+        public void Dispose()
+        {
+            if(wordDoc!=null)
+                wordDoc.Close();
+            if(wordApp!=null)
+                wordApp.Quit();
+            
+        }
+
+    }
+
+
+}
---
+++ EasyWord/WordTest/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using EasyWord;
+namespace WordTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            dialog.Description = "请选择保存的路径";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                string folder = dialog.SelectedPath;
+                EasyWord.EasyWord word=new EasyWord.EasyWord(txtFileName.Text);
+
+                word.CreateWord(folder);
+                
+            }
+        }
+    }
+}
